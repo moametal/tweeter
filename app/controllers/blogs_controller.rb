2c16400d8650ba4f064827421a3fea3e634896a8
Junya Kuroda <kuroda-j@nttpc.co.jp>
2017-11-06 02:30:09 +0000

    first commit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   
+  
   def index
     @blogs = Blog.all
   end
@@ -10,13 +11,14 @@ class BlogsController < ApplicationController
     else
       @blog = Blog.new
     end
+      
   end
 
   def create
     @blog = Blog.new(blogs_params)
     if @blog.save
       # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
-      redirect_to blogs_path, notice: "ブログを作成しました！"
+      redirect_to blogs_path, notice: "tweetを作成しました！"
     else
       # 入力フォームを再描画します。
       render 'new'
@@ -31,7 +33,7 @@ class BlogsController < ApplicationController
     @blog = Blog.find(params[:id])
     
     if @blog.update(blogs_params)
-    redirect_to blogs_path, notice: "ブログを更新しました！"
+    redirect_to blogs_path, notice: "tweetを更新しました！"
     else
       render 'edit'
     end
@@ -40,7 +42,7 @@ class BlogsController < ApplicationController
   def destroy
     @blog = Blog.find(params[:id])
     @blog.destroy
-    redirect_to blogs_path, notice: "ブログを削除しました！"
+    redirect_to blogs_path, notice: "tweetを削除しました！"
   end
   
   def confirm
